07c9f7b4fe16a0a1a1d5021be0ee8df268491f2a
Yardley Lagombra <ylagombra@yahoo.com>
2016-04-07 00:06:54 -0400

    initial commit

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -9,17 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    
+    //Outlets
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var txtBox: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var coinButton: UIButton!
+    @IBOutlet weak var score: UILabel!
+    
+    @IBAction func onCoinTap(sender: UIButton) {
+        
+        currentTaps++
+        updateTaps()
+        
+        if isGameOver()
+        {
+            restartGame()
+        }
     }
+    
+    
+    @IBAction func playButtonPressed(sender: UIButton) {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        if txtBox.text != nil && txtBox.text != ""
+        {
+        
+        logo.hidden = true
+        txtBox.hidden = true
+        playButton.hidden = true
+        
+        coinButton.hidden = false
+        score.hidden = false
+            
+        maxTaps = Int(txtBox.text!)!
+        currentTaps = 0
+        
+        updateTaps()
+            
+        }
+        
     }
-
+    func restartGame()
+    {
+        maxTaps = 0
+        txtBox.text = ""
+        
+        logo.hidden = false
+        playButton.hidden = false
+        txtBox.hidden = false
+        
+        coinButton.hidden = true
+        score.hidden = true
+        
+    }
+    
+    
+    func updateTaps(){
+        
+        score.text = "\(currentTaps) Taps"
+    }
+    
+    func isGameOver() ->Bool
+    {
+        if currentTaps >= maxTaps
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+    }
+    
 
 }
 
